553188a0399e8f82f2de310531402f785e8c541c
Chris Taylor <chris@christaylordeveloper.co.uk>
2025-09-12 10:54:29 +0700

    Update my examples and the readme.

--- Main.lean
+++ Main.lean
@@ -3,12 +3,16 @@ import RflFirstAttempt
 def main : IO Unit :=
   IO.println s!"Hello, {hello}!"
 
-theorem two_eq_two : 2 = 2 := by
-  exact rfl
+theorem two_eq_two : 2 = 2 := by exact rfl
 
-theorem two_eq_two_b : 2 = 2 := by
-  rfl
+theorem two_eq_two_b : 2 = 2 := by rfl
 
 example : 3 = 3 := by rfl
 
-def sub1 : Nat → Nat | Nat.zero => Nat.zero | succ x => x
+def sub1 : Nat → Nat | Nat.zero => Nat.zero | Nat.succ x => x
+
+example : sub1 0 = 0 := by rfl
+
+theorem zero_eq_zero : sub1 0 = 0 := by rfl
+
+example : sub1 4 = 3 := by rfl
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# rfl-first-attempt
+# Math Study Lean4
+
+My Lean4 self study notes.
