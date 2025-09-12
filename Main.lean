0ff9718a191afdf64c934e64293d430644fda4c9
Chris Taylor <chris@christaylordeveloper.co.uk>
2025-09-12 10:40:19 +0700

    Broken here.

--- Main.lean
+++ Main.lean
@@ -2,3 +2,13 @@ import RflFirstAttempt
 
 def main : IO Unit :=
   IO.println s!"Hello, {hello}!"
+
+theorem two_eq_two : 2 = 2 := by
+  exact rfl
+
+theorem two_eq_two_b : 2 = 2 := by
+  rfl
+
+example : 3 = 3 := by rfl
+
+def sub1 : Nat → Nat | Nat.zero => Nat.zero | succ x => x
