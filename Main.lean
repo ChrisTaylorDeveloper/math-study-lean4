d4ff4f7ed8c16d86518a4e19e13acca4234cbee4
Chris Taylor <chris@christaylordeveloper.co.uk>
2025-09-13 12:59:23 +0700

    Remove expressions that don't work!

--- Main.lean
+++ Main.lean
@@ -6,11 +6,6 @@ def main : IO Unit := IO.println s!"Hello, {hello}!"
 
 def sub1 : Nat → Nat | Nat.zero => Nat.zero | Nat.succ x => x
 
-def is_nat_zero (n : Nat) : Prop := n = 0
-
-#eval is_nat_zero 0 -- True
-#eval is_nat_zero 1 -- False
-
 theorem two_eq_two : 2 = 2 := by exact rfl
 
 theorem two_eq_two_b : 2 = 2 := by rfl
@@ -24,6 +19,3 @@ theorem zero_eq_zero : sub1 0 = 0 := by rfl
 example : sub1 4 = 3 := by rfl
 
 example (x : Nat) : sub1 (Nat.succ x) = x := by rfl
-
--- Example 4: Equality using a constant value
-example : is_nat_zero 0 = true := by rfl
